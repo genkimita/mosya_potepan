b83aa7e3010d0d4dfa7c8db84a5194bb2c598318
genkimita <66241426+genkimita@users.noreply.github.com>
2020-10-18 23:33:51 +0900

    Update script.js

--- course/script/script.js
+++ course/script/script.js
@@ -1,17 +1,6 @@
 $(function(){
 
-  jQuery( '#jquery-cycle' ) . cycle( {
-          fx: 'scrollHorz',
-          speed: 500,
-          timeout: 0,
-          sync: 1,
-          next: '#jquery-cycle-next',
-          prev: '#jquery-cycle-prev'
-      } );
-      jQuery( '#jquery-cycle-wrap' ) . hover(
-          function() { jQuery( '#jquery-cycle-nav' ) . fadeIn(); },
-          function() { jQuery( '#jquery-cycle-nav' ) . fadeOut(); }
-      );
+
 
 
 
