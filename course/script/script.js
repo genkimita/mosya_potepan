fc88cbf48f04ec526032f909aabf5b2e58879a62
genkimita <66241426+genkimita@users.noreply.github.com>
2020-10-19 10:50:50 +0900

    Update script.js

--- course/script/script.js
+++ course/script/script.js
@@ -1,6 +1,16 @@
 $(function(){
 
+$('.change-btn').click(function(){
+  var $displaySlide = $('.active');
+  $displaySlide.removeClass('active');
 
+if ($(this).hasClass('next-btn')) {
+  $displaySlide.next().addClass('active');
+} else {
+  $displaySlide.prev().addClass('active');
+}
+
+});
 
 
 
