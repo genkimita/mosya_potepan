ead94be2ad7337cc6c69ed188aa1d2c17cc1f1b0
genkimita <66241426+genkimita@users.noreply.github.com>
2020-10-20 18:30:08 +0900

    Update script.js

--- course/script/script.js
+++ course/script/script.js
@@ -1,6 +1,8 @@
 $(function(){
 
 $('.slides').slick({
+    prevArrow: '<img class="prev-arrow" src="image/btn-left.png" alt="" href="">',
+    nextArrow: '<img  class="next-arrow" src="image/btn-right.png" alt="" href="">',
     autoplay: false,
     autoplaySpeed: 3000,
     speed: 400,
